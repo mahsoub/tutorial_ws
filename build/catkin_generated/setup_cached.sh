dd151926c82197d45df84e88fe15886a3c272bad
mahsoub <ahmedmahsob@gmail.com>
2021-01-08 16:37:23 +0200

    compiling the broadcaster by editting CMakeLists.txt file

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/mahsoub/tutorial_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/mahsoub/tutorial_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/mahsoub/tutorial_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/mahsoub/tutorial_ws/src:$ROS_PACKAGE_PATH"
